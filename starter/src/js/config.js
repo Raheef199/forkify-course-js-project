bad4392ad7df087de5ff50cf7c5f0ff066d01b0f
raheef199 <raheef199@gmail.com>
2024-03-24 17:22:12 +0300

    Initial Commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9014fb16-0fda-4d3d-bd35-33bab06af84f';
+export const MODAL_CLOSE_SEC = 2.5;
